3f551227070d70d1a1d81c1df0b066175e37cb52
Sintry1 <75076281+Sintry1@users.noreply.github.com>
2021-12-17 13:57:08 +0100

    Update FriendList.java

--- src/main/java/com/example/servera/entities/FriendList.java
+++ src/main/java/com/example/servera/entities/FriendList.java
@@ -1,11 +1,10 @@
 package com.example.servera.entities;
 
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
+
 
 @Entity
 @NoArgsConstructor
