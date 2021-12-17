c96f135de0dc91d548dbc99278ef75b56c6e6b2c
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2021-12-17 13:51:09 +0100

    Changed IPs for the instances

--- src/main/java/com/example/servera/controllers/RequestController.java
+++ src/main/java/com/example/servera/controllers/RequestController.java
@@ -23,8 +23,8 @@ public class RequestController {
 
     private RestTemplate restTemplate = new RestTemplate();
 
-    final String homeIp = "34.235.165.180";
-    final String foreignIp = "54.210.22.88";
+    final String homeIp = "18.209.167.95";
+    final String foreignIp = "54.160.4.136";
 
     RequestService requestService;
     UserService userService;
